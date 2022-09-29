7987cb581c3be41dbd47ceea9a2c22b49644f96a
Shruti Verma <sv2611@columbia.edu>
2022-09-29 15:05:30 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.with_ratings(ratings_list)
+    if ratings_list.empty?
+      Movie.all
+    else 
+      where(rating: ratings_list)
+    end
+  end
+
+  def self.all_ratings
+    Movie.distinct.pluck(:rating)
+  end
 end
